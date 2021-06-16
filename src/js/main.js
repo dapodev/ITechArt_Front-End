07fbf3d0ee0b5d2a70a2e95ef1919ff2153a29ff
wooz1ewu <apolonik.danila2002@gmail.com>
2021-06-16 21:33:08 +0300

    Initial commit.

---
+++ src/js/main.js
@@ -0,0 +1,143 @@
+Array.prototype.map = function(callback) {
+    if(this == null) {
+        throw new TypeError('context null or not defined')
+    }
+
+    if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+    }
+
+    let newArray = new Array();
+
+    for(let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i], i, this))
+    }
+
+    return newArray;
+}
+
+Array.prototype.filter = function(callback) {
+    if(this == null) {
+        throw new TypeError('context null or not defined')
+    }
+
+    if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+    }
+    
+    let newArray = new Array();
+
+    for(let i = 0; i < this.length; i++) {
+        if(callback(this[i]) === true) {
+            newArray.push(this[i])
+        }
+    }
+
+    return newArray;
+}
+
+Array.prototype.reduce = function(callback, initialValue) {
+    if(this == null) {
+        throw new TypeError('context null or not defined')
+    }
+
+    if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+    }
+    
+    let result = initialValue === undefined ? this[0] : initialValue
+    let startIndex = initialValue === undefined ? 1 : 0
+
+    for(let i = startIndex; i < this.length; i++) {
+        result = callback(result, this[i], i, this);
+    }
+
+    return result;
+}
+
+const notes = [
+    {
+    id: 1,
+    title: 'Recipe',
+    description: 'Ingredients include 2 eggs...',
+    pagesCount: 2,
+    isMarked: false,
+    access: []
+    },
+    {
+    id: 2,
+    title: 'Workouts',
+    description: '3 sets of squats...',
+    pagesCount: 1,
+    isMarked: true,
+    access: []
+    },
+    {
+    id: 3,
+    title: 'Passwords',
+    description: 'VISA ...',
+    pagesCount: 6,
+    isMarked: true,
+    access: []
+    },
+    {
+    id: 4,
+    title: 'To Do 2021',
+    description: '1. Learn JS...',
+    pagesCount: 3,
+    isMarked: false,
+    access: []
+    }]
+
+// === T E S T I N G ===
+
+// map testing 
+
+console.log(notes)
+
+const modifiedNotes = notes.map(el => {
+    return {
+        id : el.id,
+        title : el.title
+    }
+});
+
+console.log(modifiedNotes)
+
+// filter testing
+
+const markedNotes = notes.filter(el => el.isMarked)
+
+console.log(markedNotes)
+
+// reduce testing
+
+const pagesCount = notes.reduce((prev, el) => prev + el.pagesCount, 0)
+
+console.log(pagesCount)
+
+// uniqueElement
+
+// TODO
+// - find more elegant way
+function uniqueElement(arr) {
+    let unique = []
+
+    for(let i = 0; i < arr.length; i++) {
+        let hasDuplacate = false;
+        
+        if(arr.filter(el => el === arr[i]).length > 1) {
+            hasDuplacate = true
+        }
+
+        if(!hasDuplacate) {
+            unique.push(arr[i])
+        }
+    }
+
+    return unique.length === 1 ? unique[0] : undefined
+}
+
+const testArray = [1,1,3,3,2,2,2,2,6];
+
+console.log(uniqueElement(testArray))
