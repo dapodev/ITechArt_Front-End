8e8500a6fdaf20c133110cc8a4777bd5cca264b1
wooz1ewu <apolonik.danila2002@gmail.com>
2021-06-16 22:26:01 +0300

    Document refactored using Prettier

--- task1/js/main.js
+++ task1/js/main.js
@@ -1,133 +1,136 @@
-Array.prototype.map = function(callback) {
-    if(this == null) {
-        throw new TypeError('context null or not defined')
+Array.prototype.map = function (callback) {
+  if (this == null) {
+    throw new TypeError("context null or not defined");
+  }
+
+  if (typeof callback !== "function") {
+    throw new TypeError(callback + " is not a function");
+  }
+
+  let newArray = new Array();
+
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i], i, this));
+  }
+
+  return newArray;
+};
+
+Array.prototype.filter = function (callback) {
+  if (this == null) {
+    throw new TypeError("context null or not defined");
+  }
+
+  if (typeof callback !== "function") {
+    throw new TypeError(callback + " is not a function");
+  }
+
+  let newArray = new Array();
+
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i]) === true) {
+      newArray.push(this[i]);
     }
+  }
 
-    if (typeof callback !== 'function') {
-        throw new TypeError(callback + ' is not a function');
-    }
+  return newArray;
+};
 
-    let newArray = new Array();
+Array.prototype.reduce = function (callback, initialValue) {
+  if (this == null) {
+    throw new TypeError("context null or not defined");
+  }
 
-    for(let i = 0; i < this.length; i++) {
-        newArray.push(callback(this[i], i, this))
-    }
+  if (typeof callback !== "function") {
+    throw new TypeError(callback + " is not a function");
+  }
 
-    return newArray;
-}
+  let result = initialValue === undefined ? this[0] : initialValue;
+  let startIndex = initialValue === undefined ? 1 : 0;
 
-Array.prototype.filter = function(callback) {
-    if(this == null) {
-        throw new TypeError('context null or not defined')
-    }
+  for (let i = startIndex; i < this.length; i++) {
+    result = callback(result, this[i], i, this);
+  }
 
-    if (typeof callback !== 'function') {
-        throw new TypeError(callback + ' is not a function');
-    }
-    
-    let newArray = new Array();
-
-    for(let i = 0; i < this.length; i++) {
-        if(callback(this[i]) === true) {
-            newArray.push(this[i])
-        }
-    }
-
-    return newArray;
-}
-
-Array.prototype.reduce = function(callback, initialValue) {
-    if(this == null) {
-        throw new TypeError('context null or not defined')
-    }
-
-    if (typeof callback !== 'function') {
-        throw new TypeError(callback + ' is not a function');
-    }
-    
-    let result = initialValue === undefined ? this[0] : initialValue
-    let startIndex = initialValue === undefined ? 1 : 0
-
-    for(let i = startIndex; i < this.length; i++) {
-        result = callback(result, this[i], i, this);
-    }
-
-    return result;
-}
+  return result;
+};
 
 const notes = [
-    {
+  {
     id: 1,
-    title: 'Recipe',
-    description: 'Ingredients include 2 eggs...',
+    title: "Recipe",
+    description: "Ingredients include 2 eggs...",
     pagesCount: 2,
     isMarked: false,
-    access: []
-    },
-    {
+    access: [],
+  },
+  {
     id: 2,
-    title: 'Workouts',
-    description: '3 sets of squats...',
+    title: "Workouts",
+    description: "3 sets of squats...",
     pagesCount: 1,
     isMarked: true,
-    access: []
-    },
-    {
+    access: [],
+  },
+  {
     id: 3,
-    title: 'Passwords',
-    description: 'VISA ...',
+    title: "Passwords",
+    description: "VISA ...",
     pagesCount: 6,
     isMarked: true,
-    access: []
-    },
-    {
+    access: [],
+  },
+  {
     id: 4,
-    title: 'To Do 2021',
-    description: '1. Learn JS...',
+    title: "To Do 2021",
+    description: "1. Learn JS...",
     pagesCount: 3,
     isMarked: false,
-    access: []
-    }]
+    access: [],
+  },
+];
 
 // === T E S T I N G ===
 
-// map testing 
+// map testing
 
-console.log(notes)
+console.log(notes);
 
-const modifiedNotes = notes.map(el => {
-    return {
-        id : el.id,
-        title : el.title
-    }
+const modifiedNotes = notes.map((el) => {
+  return {
+    id: el.id,
+    title: el.title,
+  };
 });
 
-console.log(modifiedNotes)
+console.log(modifiedNotes);
 
 // filter testing
 
-const markedNotes = notes.filter(el => el.isMarked)
+const markedNotes = notes.filter((el) => el.isMarked);
 
-console.log(markedNotes)
+console.log(markedNotes);
 
 // reduce testing
 
-const pagesCount = notes.reduce((prev, el) => prev + el.pagesCount, 0)
+const pagesCount = notes.reduce((prev, el) => prev + el.pagesCount, 0);
 
-console.log(pagesCount)
+console.log(pagesCount);
 
 // uniqueElement
 
 function uniqueElement(arr) {
-    let unique = []
+  let unique = [];
 
-    arr.map(
-        (el, index, thisArray) => thisArray.filter(subEl => subEl === el).length === 1 ? unique.push(el) : null
-    )
+  arr.map((el, index, thisArray) =>
+    thisArray.filter((subEl) => subEl === el).length === 1
+      ? unique.push(el)
+      : null
+  );
 
-    return unique.length === 1 ? unique[0] : undefined
+  return unique.length === 1 ? unique[0] : undefined;
 }
 
-const testArray = [1,1,3,3,2,2,2,2,6];
+const testArray = [1, 1, 3, 3, 2, 2, 2, 2, 6];
 
-console.log(uniqueElement(testArray))
+console.log(uniqueElement(testArray));
