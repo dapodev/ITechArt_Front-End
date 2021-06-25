34fb3c75db42a382077b58f68ae5bbbd6bf159aa
wooz1ewu <apolonik.danila2002@gmail.com>
2021-06-25 14:15:20 +0300

    Header + Slider finished

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+const IMAGE_WIDTH = 1230;
+const IMAGE_COUNT = 4;
+const MAX_OFFSET = (IMAGE_COUNT - 1) * IMAGE_WIDTH;
+
+const sliderLine = document.querySelector('.slider__line');
+let sliderOffset = 0;
+
+
+document.querySelector('.slider__control-arrow_right').addEventListener('click', function() {
+    sliderOffset += IMAGE_WIDTH;
+    checkOffset();
+    sliderLine.style.marginLeft = -sliderOffset + 'px';
+});
+
+document.querySelector('.slider__control-arrow_left').addEventListener('click', function() {
+    sliderOffset -= IMAGE_WIDTH;
+    checkOffset();
+    sliderLine.style.marginLeft = -sliderOffset + 'px';
+});
+
+function checkOffset() {
+    if(sliderOffset > MAX_OFFSET ) {
+        sliderOffset = 0;
+    }
+    if(sliderOffset < 0) {
+        sliderOffset = MAX_OFFSET;
+    }
+}
