f93d56a652de004f23098f5bfdbe7d594e834a8e
wooz1ewu <apolonik.danila2002@gmail.com>
2021-10-26 17:39:24 +0300

    documentation: added models, get/post requests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,7 @@
+import swaggerUI from 'swagger-ui-express';
+import swaggerDoucument from '../swagger.json';
 import express from 'express';
+import cors from 'cors';
 const app = express();
 
 import greetingRouter from './modules/greeting/routes';
@@ -6,6 +9,9 @@ import notesRouter from './modules/notes/routes';
 import requestLog from './utils/log/requestLog';
 import { PORT } from './config/constants';
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoucument));
+
+app.use(cors());
 app.use(express.json());
 
 app.use(requestLog);
